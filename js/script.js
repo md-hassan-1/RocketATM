5f9f9ee383202e3e0c26bc6fe576c03f7c7628f7
md-hassan-1 <hassanansari8533@gmail.com>
2024-06-08 15:51:35 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -24,9 +24,9 @@ $(".slider").slick({
     slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 2000,
+  autoplaySpeed: 3000,
 
-   dots: true,
+//    dots: false,
   
     
 })
